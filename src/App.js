6362c2c0be723605632d71a4927840590983807a
Evan <aqwe78965@gmail.com>
2022-09-05 15:07:34 +0800

    Finished day14 task.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,29 @@ import {
   NavLink,
   Routes,
   Route,
+  useNavigate,
 } from 'react-router-dom';
 
 const Todo = () => {
-  return <p>這是 Todo 頁面 
-  </p>;
+  return (
+    <>
+      <p>這是 Todo 頁面</p>
+      <Logout/>
+    </>
+    
+  )
 };
 const Login = () => {
   return <p>這是登入頁面</p>;
+};
+
+
+
+const Logout = () => {
+  const navigate = useNavigate()
+  return (
+    <button onClick = {() => navigate('/login')}>Log Out</button>
+  )
 };
 const Register = () => {
   return <p>這是註冊頁面</p>;
